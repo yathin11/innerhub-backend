892696e2dfd85a7ad0352fad7f8d67ff0d77a709
yathin11 <yathinm316@gmail.com>
2026-04-21 00:48:13 +0530

    connected frontend to live backend

--- server.js
+++ server.js
@@ -27,9 +27,8 @@ app.use(cors({
   credentials: true
 }));
 
-// 🔹 Middleware
-app.use(express.json());
-
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 // 🔹 Routes
 app.use("/api/products", productRoutes);
 app.use("/api/orders", orderRoutes);
