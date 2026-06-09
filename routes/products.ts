109ffc4a5ab794ff2fca9d536f49873792077722
yathin11 <yathinm316@gmail.com>
2026-06-09 20:50:50 +0530

    Add get product by id route

--- routes/products.ts
+++ routes/products.ts
@@ -3,20 +3,22 @@ import Product from "../models/Product";
 
 const router = express.Router();
 
-router.get("/", async (_req: Request, res: Response) => {
+router.get("/:id", async (req: Request, res: Response) => {
   try {
-    console.log("Product Model:", Product);
+    const product = await Product.findById(req.params.id);
 
-    const products = await Product.find();
+    if (!product) {
+      return res.status(404).json({
+        message: "Product not found",
+      });
+    }
 
-    res.json(products);
-  } catch (error: any) {
-    console.error("PRODUCT ERROR:", error);
+    res.json(product);
+  } catch (error) {
+    console.error(error);
 
     res.status(500).json({
-      message: "Error fetching products",
-      error: String(error),
-      stack: error?.stack,
+      message: "Error fetching product",
     });
   }
 });
